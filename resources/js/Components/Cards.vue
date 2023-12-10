5082dc01282fdf9ce1d50a228d008f2ba32be325
drilaexe <drilonsyla123@gmail.com>
2023-12-10 17:11:42 +0100

    Added dragable and working in modal

--- resources/js/Components/Cards.vue
+++ resources/js/Components/Cards.vue
@@ -1,23 +1,42 @@
 <script setup>
-// variable
-
-defineProps(['title','description','priority','created_at']);
-
+defineProps(["title", "description", "priority", "created_at"]);
+import {
+    PencilSquareIcon,
+    ArrowsPointingOutIcon,
+    XMarkIcon,
+} from "@heroicons/vue/24/solid";
+import { inject } from 'vue'
+const showmod = inject('showmodal')
 </script>
 
 <template>
-    <div class="w-full lg:w-1/3 p-3">
+    <div class="w-full lg:w-1/3 p-3 grid grid-cols-12 gap-1">
         <div
-            class="flex flex-col rounded overflow-hidden h-auto lg:h-32 border shadow shadow-lg" >  
-            <div  class="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col  leading-normal">
-                <div class="text-black font-bold text-xl mb-2 leading-tight flex justify-between" >
-                 <p>   {{ title }}</p> <p>#{{ priority }}</p>
+            class="flex flex-col rounded overflow-hidden h-auto lg:h-32 border shadow shadow-lg col-span-11"
+        >
+            <div
+                class="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-1 flex flex-col leading-normal"
+            >
+                <div
+                    class="text-black font-bold text-xl mb-2 leading-tight flex justify-between"
+                >
+                    <p>{{ title }}</p>
+                    <p>#{{ priority }}</p>
                 </div>
                 <p class="text-grey-darker text-base">
-                    {{description}}
+                    {{ description }}
                 </p>
             </div>
-            <p class="text-right text-sm mt-auto pe-2">{{created_at}}</p>
+            <p class="text-right text-end text-sm mt-auto pe-2">
+                {{ created_at }}
+            </p>
+        </div>
+        <div class="col-span-1">
+            <button>
+                <ArrowsPointingOutIcon class="h-6 w-6 text-dark-500 handle"  />
+            </button>
+            <button @click="showmod()"><PencilSquareIcon class="h-6 w-6 text-blue-500"  /></button>
+            <button><XMarkIcon class="h-6 w-6 text-red-500" /></button>
         </div>
     </div>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,6 +3,23 @@ import MainLayout from "@/Layouts/MainLayout.vue";
 import Cards from "@/Components/Cards.vue";
 import { Head } from "@inertiajs/vue3";
 import draggable from "vuedraggable";
+import Modal from '@/Components/Modal.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import InputError from '@/Components/InputError.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import {  ref,provide  } from 'vue';
+const editingModal = ref(false);
+
+const editModal = () => {
+    editingModal.value = true;
+    
+};
+const closeModal = () => {
+    editingModal.value = false;
+
+};
+provide('showmodal', editModal);
 </script>
 
 <template>
@@ -17,27 +34,52 @@ import draggable from "vuedraggable";
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div
+            
+                <draggable
+                :list ="$page['props']['tasks']"
+                    @start="drag = true"
+                    @end="drag = false"
+                    item-key="id"
+                    handle=".handle"
                     class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-wrap flex-col md:flex-row"
                 >
-                    <draggable
-                        v-model="$page['props']['tasks']"
-                        group="people"
-                        @start="drag = true"
-                        @end="drag = false"
-                        item-key="id"
-                    >
-                        <template #item="{ element }">
-                            <Cards
-                                v-bind:priority="element.priority"
-                                v-bind:title="element.name"
-                                v-bind:description="element.description"
-                                v-bind:created_at="element.created_at"
-                            ></Cards>
-                        </template>
-                    </draggable>
-                </div>
+                    <template #item="{ element, index }">
+                        <Cards
+                            v-bind:priority="index + 1"
+                            v-bind:title="element.name"
+                            v-bind:description="element.description"
+                            v-bind:created_at="element.created_at"
+                        ></Cards>
+                    </template>
+                </draggable>
             </div>
         </div>
+       
+        <Modal :show="editingModal" @close="closeModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900">
+                    Are you sure you want to delete your account?
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600">
+                    Once your account is deleted, all of its resources and data will be permanently deleted. Please
+                    enter your password to confirm you would like to permanently delete your account.
+                </p>
+
+                <div class="mt-6">
+                    <InputLabel for="password" value="Password" class="sr-only" />
+
+                
+
+                   
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click="closeModal"> Cancel </SecondaryButton>
+
+                  
+                </div>
+            </div>
+        </Modal>
     </MainLayout>
 </template>
